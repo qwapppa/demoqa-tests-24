720a0ddc9f33d0da34e823d13f66a59c9fd71bd5
Daniil Mikhalkov <danielionaire@gmail.com>
2024-10-14 16:05:30 +0300

    added all things and complete the test

--- src/test/java/tests/PracticeFormTests.java
+++ src/test/java/tests/PracticeFormTests.java
@@ -32,7 +32,7 @@ public class PracticeFormTests {
         $("#lastName").setValue("Mikhalkov");
         $("#userEmail").setValue("qwapppa@mail.ru");
 
-        //Радио-кнопка
+        //Проставление радио-кнопок
 
         $("label[for='gender-radio-2']").click();
         $("label[for='gender-radio-3']").click();
@@ -49,6 +49,48 @@ public class PracticeFormTests {
         $(".react-datepicker__month-select").selectOption("June");
         $(".react-datepicker__year-select").click();
         $(".react-datepicker__year-select").selectOption("2000");
+        $(".react-datepicker__day--012").click();
 
+        //Выбор меток
+
+        $("#subjectsInput").setValue("civics").pressEnter();
+
+        //Заполнение чек-боксов
+
+        $("label[for='hobbies-checkbox-1']").click();
+        $("label[for='hobbies-checkbox-2']").click();
+        $("label[for='hobbies-checkbox-3']").click();
+
+        //Загрузка файла
+
+        $("#uploadPicture").uploadFromClasspath("purple.jpg");
+
+        //Заполнение адреса
+
+        $("#currentAddress").setValue("Улица Пушкина дом Колотушкина");
+
+        //Выбор штата и города
+
+        $("#state").click();
+        $("#react-select-3-option-1").click();
+        $("#city").click();
+        $("#react-select-4-option-1").click();
+
+        //Отправка формы
+
+        $("#submit").click();
+
+        //Проверка формы на корректность
+
+        $(".modal-body").shouldHave(text("Daniil Mikhalkov"));
+        $(".modal-body").shouldHave(text("qwapppa@mail.ru"));
+        $(".modal-body").shouldHave(text("1234567890"));
+        $(".modal-body").shouldHave(text("Male"));
+        $(".modal-body").shouldHave(text("12 June,2000"));
+        $(".modal-body").shouldHave(text("Sports, Reading, Music"));
+        $(".modal-body").shouldHave(text("Civics"));
+        $(".modal-body").shouldHave(text("Улица Пушкина дом Колотушкина"));
+        $(".modal-body").shouldHave(text("Uttar Pradesh Lucknow"));
+        $(".modal-body").shouldHave(text("purple.jpg"));
     }
 }
